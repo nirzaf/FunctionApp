ac7f5a01ab95b3b37c438f5675c8c368227bef91
nirzaf <mfmfazrin1986@gmail.com>
2023-10-10 18:29:12 +0300

    Remove BlobTrigger, modify QueueTrigger, and add new dependencies
    Deleted BlobTrigger.cs since Blob trigger functionality is phased out. QueueTrigger.cs is modified to use the updated QueueTrigger attribute from Microsoft.Azure.Functions.Worker SDK. New dependencies were added in FunctionApp.csproj to support the latest Azure Functions Worker SDK. Further, global.json and .gitignore files are added to the project to manage SDK versions and ignore unnecessary files in the git repository respectively.

--- FunctionApp/Triggers/QueueTrigger.cs
+++ FunctionApp/Triggers/QueueTrigger.cs
@@ -6,9 +6,9 @@ namespace FunctionApp.Triggers
     public class QueueTrigger
     {
         [FunctionName("QueueTrigger")]
-        public void Run([QueueTrigger("localqueue", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log)
+        public void Run([Microsoft.Azure.Functions.Worker.QueueTrigger("localqueue", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log)
         {
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+            log.LogInformation("C# Queue trigger function processed: {MyQueueItem}", myQueueItem);
         }
     }
 }
